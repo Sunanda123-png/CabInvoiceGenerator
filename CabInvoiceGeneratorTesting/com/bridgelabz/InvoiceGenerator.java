198466fc473148a6f0f29fd9859b01db9614868c
Sunanda123-png <sunanda02@gmail.com>
2021-11-08 01:28:11 +0530

    Add:Calculate total fare for given distance and time

---
+++ CabInvoiceGeneratorTesting/com/bridgelabz/InvoiceGenerator.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+
+/**
+ * Author:- Sunanda Shil
+ * date:-05/11/21
+ */
+
+public class InvoiceGenerator {
+    private static final double MINIMUM_COST_PER_KILLOMETER = 10;
+    private static final int COST_PER_TIME = 1;
+    private static final double MINIMUM_FARE = 5;
+
+    public double calculateFare(double distance, int time) {
+        double totalFare = distance * MINIMUM_COST_PER_KILLOMETER + time * COST_PER_TIME;
+        if(totalFare < MINIMUM_FARE)
+            return MINIMUM_FARE;
+        return totalFare;
+    }
+}
