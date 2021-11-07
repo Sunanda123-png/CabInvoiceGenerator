920c178096d563c33b93cd2fdacb41ef8518fb13
Sunanda123-png <sunanda02@gmail.com>
2021-11-08 02:28:07 +0530

    Add:Calculate total fare for multiple rides

--- CabInvoiceGeneratorTesting/com/bridgelabz/CabInvoiceTest.java
+++ CabInvoiceGeneratorTesting/com/bridgelabz/CabInvoiceTest.java
@@ -12,4 +12,21 @@ public class CabInvoiceTest {
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assertions.assertEquals(25, fare, 0.0);
     }
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinimumFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double minimumFare = invoiceGenerator.calculateFare(distance, time);
+        Assertions.assertEquals(5, minimumFare, 0.0);
+    }
+    @Test
+    public void givenMultipleRides_ShouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        Ride[] rides = { new Ride(2.0, 5),
+                new Ride(0.1, 1),
+        };
+        double fare = invoiceGenerator.calculateFare(rides);
+        Assertions.assertEquals(30, fare, 0.0);
+    }
 }
