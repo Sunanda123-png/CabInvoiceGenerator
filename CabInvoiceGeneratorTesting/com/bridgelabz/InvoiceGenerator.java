ace88ef68788ec600ef030cd301caede79fce7a0
Sunanda123-png <sunanda02@gmail.com>
2021-11-08 02:56:55 +0530

    ADD:Invoice summary for example number of ride,total fare,Avarage fare

--- CabInvoiceGeneratorTesting/com/bridgelabz/InvoiceGenerator.java
+++ CabInvoiceGeneratorTesting/com/bridgelabz/InvoiceGenerator.java
@@ -12,19 +12,15 @@ public class InvoiceGenerator {
 
     public double calculateFare(double distance, int time) {
         double totalFare = distance * MINIMUM_COST_PER_KILLOMETER + time * COST_PER_TIME;
-        if(totalFare < MINIMUM_FARE)
-            return MINIMUM_FARE;
-        return totalFare;
+        return Math.max(totalFare, MINIMUM_FARE);
     }
 
-    public double calculateFare(Ride[] rides) {
+    public InvoiceSummary calculateFare(Ride[] rides) {
         double totalFare = 0;
         for (Ride ride:rides){
             totalFare += this.calculateFare(ride.distance, ride.time);
         }
-        if(totalFare < MINIMUM_FARE)
-            return MINIMUM_FARE;
-        return totalFare;
+        return new InvoiceSummary(rides.length, totalFare);
     }
 
 }
